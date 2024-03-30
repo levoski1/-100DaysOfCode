ca53595811916110da9d651cb78616c4bc2be620
Ugwoke levi <113690452+levoski1@users.noreply.github.com>
2024-03-30 23:07:07 +0100

    pld

--- to_do_list.py
+++ to_do_list.py
@@ -21,6 +21,32 @@ def add_student(filename,add):
     except FileNotFoundError:
         print('File Not found')
 
+        
+def edit_student(filename, remove, replace):
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            modified_lines = []
+
+            for line in lines:
+                if line.strip() == remove:
+                    modified_lines.append(replace + '\n')  # Append the replacement content in a new line
+                    print(f'Successfully Edited {remove} to {replace}')
+                else:
+                    modified_lines.append(line)
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.writelines(modified_lines)
+
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+            
+
+    except FileNotFoundError:
+        print(f'Sorry! File doesn\'t exist')
 
 def delete_student(filename,delete):
     try:
@@ -31,21 +57,23 @@ def delete_student(filename,delete):
             for line in lines:
                 if line.strip() != delete:
                     empty_line.append(line)
+                    
 
                 else:
-                    print(f'Sussfully deleted {line}')
+                    print(f'Sussfully deleted {LSline}')
                     del line
+                
 
         with open(filename,'w',encoding='utf-8') as f:
             f.writelines(empty_line)   
             
 
     except FileNotFoundError:
-        print(f'Sorry! File doesn\'t exist')
+        print('Sorry! File doesn\'t exist')
 
 
 
-delete_student('ext.txt','somto')
+
 
 filename = 'ext.txt'
 arg = sys.argv
@@ -57,18 +85,27 @@ try:
     elif int(arg[1]) == 2:
         add = input('What do you want to add? ')
         add_student(filename,add)
-
+        
     elif int(arg[1]) == 3:
+        modify = input('Who do you want to modifiy? ')
+        replace = input('Who do you want to replace? ')
+
+        edit_student(filename,modify,replace)
+
+    
+    elif int(arg[1]) == 4:
         delete = input('Who do you want to delete? ')
         delete_student(filename,delete)
 
-except Exception as e:
-    print(e)
+except:
+    print("\n")
     print('''
                     Available Option
     1. view Student
     2. Add student
-    3. Delete student
+    3. Edit Student
+    4. Delete student
           
 ''')
+    print("hint: python file_name 3")
 
